a805e4214850e4319de8eb643574c2752123d992
Vaibhav <vkpgpay@gmail.com>
2025-02-19 12:18:21 +0530

    queue implementation using array

---
+++ Lecture26_Feb19_QueueUsingArrayAndLinkedList/ArrayQueueClient.java
@@ -0,0 +1,25 @@
+package Lecture26_Feb19_QueueUsingArrayAndLinkedList;
+
+public class ArrayQueueClient {
+    public static void main(String[] args) {
+        ArrayQueue q = new ArrayQueue();
+
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+
+        q.display();
+
+        System.out.println("Size of queue: " + q.size());
+        System.out.println("Deleting front: " + q.dequeue());
+        System.out.println("New front: " + q.peek());
+        System.out.println("New size of queue: " + q.size());
+
+        q.dequeue();
+        q.dequeue();
+
+        System.out.println("Deleted 2 elements.");
+
+        q.display();
+    }
+}
