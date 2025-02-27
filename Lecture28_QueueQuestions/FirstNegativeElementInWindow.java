f85a179af69455158edd0281ef9a121e49cc3b04
Vaibhav <vkpgpay@gmail.com>
2025-02-27 15:09:19 +0530

    first negative element in a window of size K

---
+++ Lecture28_QueueQuestions/FirstNegativeElementInWindow.java
@@ -0,0 +1,45 @@
+package Lecture28_QueueQuestions;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class FirstNegativeElementInWindow {
+
+    static int[] firstNegative(int[] arr, int k) {
+
+        int n = arr.length, idx = 0;
+        int[] neg = new int[n - k + 1];
+
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+
+            // no non-negative integer can be a valid answer
+            if (arr[i] < 0)
+                dq.add(i);
+
+            // if the peekfirst is out of window, i.e, it does not belong to [i-k+1, i]
+            if (!dq.isEmpty() && dq.peekFirst() < i - k + 1)
+                dq.removeFirst();
+
+            // as first window will be at i = k-1
+            if (i >= k - 1) {
+                if (dq.isEmpty())
+                    neg[idx++] = 0;
+                else
+                    neg[idx++] = arr[dq.peekFirst()];
+            }
+        }
+        return neg;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, -1, 3, -3, 4, -6, 7, 8, 9 };
+
+        int[] neg = firstNegative(arr, 3);
+
+        for (int i : neg)
+            System.out.print(i + " ");
+
+    }
+}
